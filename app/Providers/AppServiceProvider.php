cddf6c8ab789b1085bfaa0473c282067b00bfb2a
rifki ahmad sururi <rifkisururi27@gmail.com>
2021-04-17 10:03:20 +0700

    env heroku must https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->environment('heroku')) {
-            URL::forceSchema('https');
-        }
+        //
     }
 }
