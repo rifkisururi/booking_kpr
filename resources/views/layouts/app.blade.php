385d3443597474ae0d40d039fa8110c9037f0e97
rifki ahmad sururi <57984062+rifkisururi@users.noreply.github.com>
2021-04-17 09:06:46 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
         <link href="{{ secure_asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
         <link href="{{ secure_asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
         <!-- Argon CSS -->
-        <link type="text/css" href="{{ asset('argon') }}/css/argon.css" rel="stylesheet">
+        <link type="text/css" href="{{ secure_asset('argon') }}/css/argon.css" rel="stylesheet">
     </head>
     <body class="{{ $class ?? '' }}">
         @auth()
